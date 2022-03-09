dc347f87bb2bb08e9ca99e16d49266b42f20302d
Sacha Defrère <sachadefrere@gmail.com>
2022-03-09 13:19:39 +0100

    debug coming

--- Assignment 2/pagecollect.py
+++ Assignment 2/pagecollect.py
@@ -1,18 +1,20 @@
 from __future__ import annotations
 from search import *
-from scipy.special import factorial
+import time
 
 ###############
 # State class #
 ###############
 class State:
-    def __init__(self, grid: list, cost: int=0, heuristic: float=0.0, player_pos: tuple=(0, 0)) -> None:
+    def __init__(self, grid: list, cost: int=0, player_pos: tuple=(0, 0), n_pages: int=0, pages_pos: list=[], heuristic: int=0) -> None:
         self.nbr = len(grid)
         self.nbc = len(grid[0])
         self.grid = grid
         self.cost = cost
-        self.heuristic = heuristic
         self.player_pos = player_pos
+        self.n_pages = n_pages
+        self.pages_pos = pages_pos
+        self.heuristic = heuristic
 
     def __str__(self) -> str:
         return '\n'.join(''.join(row) for row in self.grid)
@@ -39,6 +41,13 @@ class State:
 def dist_man(pos1, pos2) -> int:
         return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
 
+def h(state: State, final_pos: tuple) -> int:
+    h = 0
+    for i in range(state.n_pages):
+        h += dist_man(state.player_pos, state.pages_pos[i])
+    h += dist_man(state.player_pos, final_pos)
+    return h
+
 
 #################
 # Problem class #
@@ -47,8 +56,8 @@ class PageCollect(Problem):
 
     def __init__(self, initial: State) -> None:
         final_goal = []
-        self.n_pages = 0
-        self.pages_pos = []
+        n_pages = 0
+        pages_pos = []
         for nth_row in range(initial.nbr):
             goal_row = []
             for nth_col in range(initial.nbc):
@@ -57,15 +66,17 @@ class PageCollect(Problem):
                     initial.player_pos = (nth_col, nth_row)
                 elif initial.grid[nth_row][nth_col] == "p":
                     goal_row.append(" ")
-                    self.n_pages += 1
-                    self.pages_pos.append((nth_col, nth_row))
+                    n_pages += 1
+                    pages_pos.append((nth_col, nth_row))
                 elif initial.grid[nth_row][nth_col] == "X":
                     goal_row.append("@")
                     self.goal_pos = (nth_col, nth_row)
                 else:
                     goal_row.append(initial.grid[nth_row][nth_col])
             final_goal.append(goal_row)
-        goal = State(final_goal, 0, 0, self.goal_pos)
+        initial.n_pages = n_pages
+        initial.pages_pos = pages_pos
+        goal = State(final_goal, 0, self.goal_pos, 0, [], 0)
         super().__init__(initial, goal=goal)
         # print(initial.grid[2])
         # print(initial)
@@ -110,31 +121,33 @@ class PageCollect(Problem):
             new_x, new_y = old_x, old_y+1
         elif action == 'n':
             new_x, new_y = old_x, old_y-1
-        if action == 'e':
+        elif action == 'e':
             new_x, new_y = old_x+1, old_y
         elif action == 'w':
             new_x, new_y = old_x-1, old_y
         
+        n_pages = state.n_pages
+        pages_pos = state.pages_pos
         if grid[new_y][new_x] == "p":
-            self.n_pages -= 1
-            del self.pages_pos[self.pages_pos.index((new_x, new_y))]
+            n_pages -= 1
+            pages_pos.remove((new_x, new_y))
         
         grid[old_y][old_x] = " "
         grid[new_y][new_x] = "@"
 
-        return State(grid, state.cost+1, self.h(), (new_x, new_y))
+        new_state = State(grid, state.cost+1, (new_x, new_y), n_pages, pages_pos, 0)
+        new_state.heuristic = h(new_state, self.goal_pos)
+        return new_state
 
     def goal_test(self, state: State) -> bool:
         return state == self.goal
     
-    def h(self, node: Node) -> float:
-        h = 0.0
-        for i in range(self.n_pages):
-            h += dist_man(node.state.player_pos,self.pages_pos[i])
+    def h(self, node: Node) -> int:
+        h = 0
+        for i in range(node.state.n_pages):
+            h += dist_man(node.state.player_pos,node.state.pages_pos[i])
         h += dist_man(node.state.player_pos,self.goal_pos)
         return h
-    
-    
 
     def load(path):
         with open(path, 'r') as f:
@@ -150,7 +163,10 @@ class PageCollect(Problem):
 #####################
 problem = PageCollect.load(sys.argv[1])
 
-node = astar_search(problem)
+# Example of search
+start_timer = time.perf_counter()
+node, nb_explored, remaining_nodes = astar_search(problem)
+end_timer = time.perf_counter()
 
 # example of print
 path = node.path()
